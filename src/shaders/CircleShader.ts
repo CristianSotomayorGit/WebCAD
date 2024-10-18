cd1b558749daae7ad478b9730353d45ac63f0f3d
cristian sotomayor <csotomayorcompsci@gmail.com>
2024-10-17 23:23:39 -0700

    basic circle functionality

---
+++ src/shaders/CircleShader.ts
@@ -0,0 +1,31 @@
+export enum CircleShader {
+    FRAGMENT =
+    `
+@fragment
+fn main() -> @location(0) vec4<f32> {
+  return vec4<f32>(1.0, 1.0, 1.0, 1.0);
+}
+`,
+    VERTEX = `
+
+    struct Uniforms {
+      cameraOffset: vec2<f32>,
+      zoomFactor: f32,
+      padding: f32,
+    }
+
+    @group(0) @binding(0) var<uniform> uniforms: Uniforms;
+
+    struct VertexOutput {
+      @builtin(position) position: vec4<f32>,
+    }
+
+    @vertex
+    fn main(@location(0) position: vec2<f32>) -> VertexOutput {
+      var output: VertexOutput;
+      let pos = (position - uniforms.cameraOffset) * uniforms.zoomFactor;
+      output.position = vec4<f32>(pos, 0.0, 1.0);
+      return output;
+    }
+    `
+}
