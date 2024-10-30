319ee1d7ff20d868c7d31fb53ad2ad377e95add6
cristian sotomayor <csotomayorcompsci@gmail.com>
2024-10-30 10:54:53 -0700

    removed constraint code from webgpucanvas

--- src/components/WebGPUCanvas.tsx
+++ src/components/WebGPUCanvas.tsx
@@ -4,20 +4,13 @@ import { EntityManager } from '../domain/managers/EntityManager';
 import { ToolManager } from '../domain/managers/ToolManager';
 import CommandToolbar from './CommandToolbar';
 import ButtonToolbar from './ButtonToolbar';
-import { ConstraintType } from '../domain/constraints/ConstraintTypes';
-import { ConstraintManager } from '../domain/managers/ConstraintManager';
-import ConstraintToolbar from './ConstraintToolbar';
 
 const WebGPUCanvas: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const rendererRef = useRef<Renderer>();
   const entityManagerRef = useRef(new EntityManager());
   const toolManagerRef = useRef<ToolManager>();
-  const constraintManagerRef = useRef<ConstraintManager>();
-
   const [activeToolName, setActiveToolName] = useState('Select');
-  const [isSnapChecked, setIsSnapChecked] = useState(false);
-  const [isOrthoChecked, setIsOrthoChecked] = useState(false);
 
   useEffect(() => {
     const initRenderer = async () => {
@@ -29,11 +22,8 @@ const WebGPUCanvas: React.FC = () => {
 
         await rendererRef.current.initialize();
 
-        constraintManagerRef.current = new ConstraintManager(entityManagerRef.current, rendererRef.current);
-
         toolManagerRef.current = new ToolManager(
           entityManagerRef.current,
-          // constraintManagerRef.current,
           rendererRef.current
         );
 
@@ -76,58 +66,8 @@ const WebGPUCanvas: React.FC = () => {
   };
 
   const handleKeyDown = (event: KeyboardEvent) => {
-    if (!constraintManagerRef.current || !toolManagerRef.current) return;
-
-    switch (event.key.toLowerCase()) {
-      case 'l':
-        setActiveToolName('Line'); // Update UI state
-        toolManagerRef.current.setActiveTool('Line'); // Update tool manager
-        break;
-
-      case 's':
-        setActiveToolName('Select'); // Update UI state
-        toolManagerRef.current.setActiveTool('Select'); // Update tool manager
-        break;
-
-      case 'p':
-        setActiveToolName('Polygon'); // Update UI state
-        toolManagerRef.current.setActiveTool('Polygon'); // Update tool manager
-        break;
-
-      case 'c':
-        setActiveToolName('Circle'); // Update UI state
-        toolManagerRef.current.setActiveTool('Circle'); // Update tool manager
-        break;
-
-      case 'y': // Changed from 's' to 'y' to avoid conflict with 'Select'
-        setActiveToolName('Spline'); // Update UI state
-        toolManagerRef.current.setActiveTool('Spline'); // Update tool manager
-        break;
-
-      case 'r':
-        setActiveToolName('Rectangle'); // Update UI state
-        toolManagerRef.current.setActiveTool('Rectangle'); // Update tool manager
-        break;
-
-      case 'e':
-        setActiveToolName('Ellipse'); // Update UI state
-        toolManagerRef.current.setActiveTool('Ellipse'); // Update tool manager
-        break;
-
-      case 'shift' && 'z':
-        setIsOrthoChecked((prev) => !prev); // Toggle Ortho checkbox state
-        constraintManagerRef.current.toggleConstraint(ConstraintType.Orthogonal, 'both');
-        break;
-
-      case 'f3' && 'x':
-        setIsSnapChecked((prev) => !prev); // Toggle Snap checkbox state
-        constraintManagerRef.current.toggleConstraint(ConstraintType.Snap, 'both');
-        break;
-
-      default:
-        toolManagerRef.current.getActiveTool()?.onKeyDown!(event);
-        break;
-    }
+    if (!toolManagerRef.current) return;
+    toolManagerRef.current.getActiveTool()?.onKeyDown!(event);
   };
 
   const handleWheel = (event: WheelEvent) => {
@@ -141,13 +81,6 @@ const WebGPUCanvas: React.FC = () => {
       <ButtonToolbar
         toolManagerRef={toolManagerRef}
         setActiveToolName={setActiveToolName}
-      />
-      <ConstraintToolbar
-        constraintToolbarRef={constraintManagerRef}
-        isSnapChecked={isSnapChecked}
-        setIsSnapChecked={setIsSnapChecked}
-        isOrthoChecked={isOrthoChecked}
-        setIsOrthoChecked={setIsOrthoChecked}
       />
       <canvas ref={canvasRef} style={{ display: 'block', overflow: 'hidden' }} />
     </>
