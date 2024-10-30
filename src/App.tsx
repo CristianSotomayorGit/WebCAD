0901e83bd5d1040bdb4fa28f5517184f5a33acf3
cristian sotomayor <csotomayorcompsci@gmail.com>
2024-10-30 10:26:24 -0700

    fixed routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,13 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './components/HomePage';
 import WebGPUCanvas from './components/WebGPUCanvas';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/app" element={<WebGPUCanvas />} />
+        <Route path="/" element={<WebGPUCanvas />} />
       </Routes>
     </Router>
   );
