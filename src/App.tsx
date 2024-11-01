7fd4f700e5276fb6aa78f0ffc5151c09a9686b74
cristian sotomayor <csotomayorcompsci@gmail.com>
2024-10-31 20:42:30 -0700

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/OtterCAD/" element={<WebGPUCanvas />} />
+        <Route path="/" element={<WebGPUCanvas />} />
       </Routes>
     </Router>
   );
