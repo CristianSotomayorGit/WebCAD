28a510f5f6cd94e1335a925bad408e29844bd82d
cristia nsotomayor <cristiansotomayorgit>
2024-11-13 10:59:13 -0800

    removed more vestiges

--- src/constants/ToolConstants.ts
+++
@@ -1,5 +0,0 @@
-// src/constants/ToolConstants.ts
-
-export const PAN_SPEED = 0.00125;
-export const ZOOM_FACTOR = 1.1;
-export const DEFAULT_NUM_SEGMENTS = 50;
--- src/types/GridStyle.ts
+++
@@ -1,7 +0,0 @@
-// src/types/GridStyle.ts
-
-export enum GridStyle {
-    Lines = 'Lines',
-    PolkaDots = 'PolkaDots',
-  }
-  
